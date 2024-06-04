cb6b0045df049eb391738af377ed8eab86d287ee
Jean Carlos <jean522010@hotmail.com>
2024-06-03 22:46:41 -0300

    Exemplo de Tipos de Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,31 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+       // A estrutura padrão para se declarar uma variável sempre será: <Tipo> <nomeVariavel> <atribuicaoDeValorOpcional>
+
+       //EXEMPLOS
+
+       int idade; //Tipo "int", nome "idade", sem valor atribuido
+       int anoNascimento = 1996; //Tipo "int", nome "anoNascimento", com valor 1996
+       double salarioMinimo = 2500; //Tipo "Double", nome "salarioMinimo", com valor de 2500
+
+
+
+        //TIPOS PRIMITIVOS
+
+       byte numero = 123;
+       short ano = 2021;
+       int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+       long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+       float pi = 3.14F;
+       double salario = 1275.33;
+       char letra = 'A';
+       boolean verdadeiro = true;
+       boolean falso = false;
+
+        
     }
+
+
+    
 }
