c6f83cbfb9fc2c6033cee7ebdecb96165b5ae0f2
Jean Carlos <jean522010@hotmail.com>
2024-07-04 21:52:14 -0300

    Exceções

---
+++ condicionais-com-java/condicionais/src/Exceções/AboutMe.java
@@ -0,0 +1,39 @@
+package Exceções;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        try{
+
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite o seu nome: ");
+            String nome = scanner.next();
+            
+            System.out.println("Digite o seu sobrenome: ");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite a sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite a sua altura: ");
+            double altura = scanner.nextDouble();
+        
+
+
+            
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase(null));
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();  
+
+        }
+        catch(InputMismatchException e ){
+            System.out.println("O campo idade e altura precisam ser numericos");
+        }
+    }
+}
